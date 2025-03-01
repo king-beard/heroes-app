948a4c88c69cf7bba5c9e111f3361454301b88af
king-beard <chucho_herrera10@icloud.com>
2025-03-01 00:15:25 -0600

    navigate push and replace

--- src/ui/components/Navbar.jsx
+++ src/ui/components/Navbar.jsx
@@ -1,6 +1,10 @@
-import { Link, NavLink } from 'react-router-dom';
+import { Link, NavLink, useNavigate } from 'react-router-dom';
 
 export const Navbar = () => {
+  const navigate = useNavigate();
+
+  const onLogout = () => navigate('/login')
+  
   return (
     <nav className="navbar navbar-expand-sm navbar-light bg-light border-bottom p-2">
 
@@ -40,7 +44,7 @@ export const Navbar = () => {
       <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
         <ul className="navbar-nav ml-auto">
           <span className="nav-item nav-link text-danger"> Jesus </span>
-          <button className="nav-item nav-link btn"> Logout </button>
+          <button className="nav-item nav-link btn" onClick={ onLogout }> Logout </button>
         </ul>
       </div>
     </nav>
