571cabf4e01072862bb5c061165279f2a8d9b091
king-beard <chucho_herrera10@icloud.com>
2025-03-05 00:50:09 -0600

    user active

--- src/ui/components/Navbar.jsx
+++ src/ui/components/Navbar.jsx
@@ -1,9 +1,15 @@
+import { useContext } from 'react';
 import { Link, NavLink, useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../auth/context';
 
 export const Navbar = () => {
+  const { user } = useContext( AuthContext )
+
   const navigate = useNavigate();
 
-  const onLogout = () => navigate('/login')
+  const onLogout = () => {
+    navigate('/login')
+  }
   
   return (
     <nav className="navbar navbar-expand-sm navbar-light bg-light border-bottom p-2 mb-2">
@@ -43,7 +49,7 @@ export const Navbar = () => {
 
       <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
         <ul className="navbar-nav ml-auto">
-          <span className="nav-item nav-link text-danger"> Jesus </span>
+          <span className="nav-item nav-link text-danger"> {user?.name} </span>
           <button className="nav-item nav-link btn" onClick={ onLogout }> Logout </button>
         </ul>
       </div>
