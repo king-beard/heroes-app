780c4497c400ce33be2e1b6e9b94ab1a64f9f3fc
king-beard <chucho_herrera10@icloud.com>
2025-03-22 20:54:55 -0600

    public and private routes

--- src/router/RouterApp.jsx
+++ src/router/RouterApp.jsx
@@ -2,13 +2,23 @@ import { Fragment } from "react"
 import { Route, Routes } from "react-router-dom"
 import { LoginPage } from "../auth/pages"
 import { HeroesRoutes } from "../heroes/routes"
+import { PrivateRouter } from "./PrivateRouter"
+import { PublicRouter } from "./PublicRouter"
 
 export const RouterApp = () => {
   return (
     <Fragment>
       <Routes>
-        <Route path="login" element={ <LoginPage /> } />
-        <Route path="/*" element={ <HeroesRoutes /> } />
+        <Route path="login" element={ 
+          <PublicRouter> 
+            <LoginPage />
+          </PublicRouter> 
+        } />
+        <Route path="/*" element={ 
+          <PrivateRouter> 
+            <HeroesRoutes />
+          </PrivateRouter> 
+        } />
       </Routes>
     </Fragment>
   )
