e0f4ce7cb103043efb7f5dba5755ab71448ed861
king-beard <chucho_herrera10@icloud.com>
2026-02-10 23:57:13 -0600

    paging component

--- src/components/custom/CustomPagination.tsx
+++ src/components/custom/CustomPagination.tsx
@@ -1,17 +1,35 @@
+import { useSearchParams } from "react-router";
+
 import { Button } from "@/components/ui/button"
 
-import { ChevronLeft, ChevronRight, MoreHorizontal } from "lucide-react"
+import { ChevronLeft, ChevronRight } from "lucide-react"
 
 interface Props {
   totalPages: number;
 }
 
-const page = 2;
 
 export const CustomPagination = ({ totalPages }: Props) => {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const queryPage = searchParams.get('page') ?? '1';
+  const page = isNaN(+queryPage) ? 1 : +queryPage;
+
+  const handlePageChange = (page: number) => {
+    if (page < 1 || page > totalPages) return;
+
+    searchParams.set('page', page.toString());
+    setSearchParams(searchParams);
+  }
+
   return (
     <div className="flex items-center justify-center space-x-2">
-      <Button variant="outline" size="sm" disabled>
+      <Button
+        variant="outline"
+        size="sm"
+        disabled={page === 1}
+        onClick={() => handlePageChange(page - 1)}
+      >
         <ChevronLeft className="h-4 w-4" />
         Previous
       </Button>
@@ -21,8 +39,9 @@ export const CustomPagination = ({ totalPages }: Props) => {
           return (
             <Button
               key={index}
-              variant={page == index ? "default" : "outline"}
+              variant={page == index + 1 ? "default" : "outline"}
               size="sm"
+              onClick={() => handlePageChange(index + 1)}
             >
               {index + 1}
             </Button>
@@ -30,20 +49,11 @@ export const CustomPagination = ({ totalPages }: Props) => {
         })
       }
 
-      {/* <Button variant="default" size="sm">
-        1
-      </Button>
-      <Button variant="outline" size="sm">
-        2
-      </Button>
-      <Button variant="outline" size="sm">
-        3
-      </Button>
-      <Button variant="ghost" size="sm" disabled>
-        <MoreHorizontal className="h-4 w-4" />
-      </Button> */}
-
-      <Button variant="outline" size="sm">
+      <Button
+        variant="outline"
+        size="sm"
+        onClick={() => handlePageChange(page + 1)}
+      >
         Next
         <ChevronRight className="h-4 w-4" />
       </Button>
--- src/heroes/pages/home/HomePage.tsx
+++ src/heroes/pages/home/HomePage.tsx
@@ -24,7 +24,7 @@ export const HomePage = () => {
   }, [activeTab]);
 
   const { data: heroesResponse } = useQuery({
-    queryKey: ['heroes'],
+    queryKey: ['heroes', { page, limit }],
     queryFn: () => getHeroesByPageAction(+page, +limit),
     staleTime: 1000 * 60 * 5
   });
@@ -103,7 +103,7 @@ export const HomePage = () => {
       </Tabs>
 
       {/* Pagination */}
-      <CustomPagination totalPages={10} />
+      <CustomPagination totalPages={heroesResponse?.pages ?? 1} />
     </>
   )
 }
